d22c21fcd21516b802b597c7b4245069222a004e
Miguel Chavez <toonchavez8@gmail.com>
2023-08-13 18:41:59 -0600

    removed unused code

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,6 @@
 import "../css/style.css";
 
-// Update these paths based on your actual directory structure
-const facebookIconPath = "/icon-facebook.svg";
-const twitterIconPath = "./assets/icon-twitter.svg";
-const pinterestIconPath = "/icon-pinterest.svg";
-
 const shareBtn = document.getElementById("shareBtn");
-const cardFooter = document.getElementById("cardFooter");
 
 // Add event listener
 shareBtn.addEventListener("click", function (event) {
@@ -14,35 +8,12 @@ shareBtn.addEventListener("click", function (event) {
 
 	// we get our share banner
 	const shareBanner = document.getElementById("shareBanner");
-	if (shareBanner) {
-		toggleShareBanner();
-	} else {
-		createShareBanner();
-	}
+	if (shareBanner) toggleShareBanner();
 });
 
 function toggleShareBanner() {
 	const shareBanner = document.getElementById("shareBanner");
 	shareBanner.classList.toggle("hidden");
-}
-
-function createShareBanner() {
-	const shareBanner = document.createElement("nav");
-	shareBanner.id = "shareBanner";
-	shareBanner.classList.add("hidden");
-	shareBanner.innerHTML = `
-    <p class="font-manrope uppercase tracking-widest text-fem-blue-300 font-normal text-sm">share</p>
-  <a href="#" class="block mb-2">
-    <img src="${facebookIconPath}" alt="Facebook" class="w-6 h-6">
-  </a>
-  <a href="#" class="block mb-2">
-    <img src="${twitterIconPath}" alt="Twitter" class="w-6 h-6">
-  </a>
-  <a href="#" class="block">
-    <img src="${pinterestIconPath}" alt="Instagram" class="w-6 h-6">
-  </a>
-`;
-	cardFooter.appendChild(shareBanner);
 }
 
 // Click event listener for the document to hide the share banner when clicked outside
